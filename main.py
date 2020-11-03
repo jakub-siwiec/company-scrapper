3811894cb6d9825f360d9731a2803e42789720f0
jakub-siwiec <jakub.siwiec@svcais.com>
2020-11-03 00:46:35 +0000

    Search from the main site added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 PATH = "./chromedriver"
 driver = webdriver.Chrome(PATH)
@@ -17,5 +18,14 @@ WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.XPATH, "//*[@id='introAgreeButton']/span"))
 ).click()
 
+# Search
+
+input_element = WebDriverWait(driver, 5).until(
+    EC.visibility_of_element_located((By.NAME, "q"))
+)
+input_element.send_keys("Blablabla")
+input_element.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.implicitly_wait(10)
 
 driver.close()
