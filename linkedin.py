91dbf64a5bd3dfb5842de454e7a5dbfe86d38ae5
jakub-siwiec <jakub.siwiec@svcais.com>
2020-11-09 12:43:48 +0000

    Linkedin scrap works

--- linkedin.py
+++ linkedin.py
@@ -4,89 +4,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from decouple import config
+import pandas as pd
 import time
-"""
-PATH = "./chromedriver"
-driver = webdriver.Chrome(PATH)
-driver.get("https://www.linkedin.com/login")
-a = input("kk")
-
-# LOGIN
-
-driver.find_element_by_id('username').send_keys(config('LINKEDIN_EMAIL'))
-driver.find_element_by_id('password').send_keys(config('LINKEDIN_PASSWORD'))
-driver.find_element_by_xpath(
-    "//button[@data-litms-control-urn='login-submit']").click()
-b = input("bb")
-
-# SEARCH
-
-search = driver.find_element_by_xpath(
-    "//div[@id='global-nav-search']//input[@aria-label='Search']")
-search.send_keys("Orchard Global Asset Management LLP")
-search.send_keys(Keys.ENTER)
-c = input("ccc")
-
-# GET RESULTS
-
-results = []
-body = driver.find_element_by_css_selector('body')
-
-while True:
-    while driver.execute_script(
-            'return ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2);') == False:
-        body.send_keys(Keys.PAGE_DOWN)
-
-    try:
-        next_button = driver.find_element_by_xpath(
-            "//button[@aria-label='Next']")
-        time.sleep(2)
-        res_i = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located(
-                (By.XPATH, "//*[@data-test-search-result='PROFILE']")))
-        print("Length: ", len(res_i))
-        print("0th element: ", res_i[0])
-        print("0th element text: ", res_i[0].text)
-        for i in res_i:
-            results.append(i.text)
-        # NEXT PAGE
-
-        if next_button.is_enabled():
-            time.sleep(2)
-            next_button.click()
-            time.sleep(2)
-        else:
-            break
-    except:
-        pass
-
-e = input("eee")
-print("--------")
-# PRINT RESULTS
-
-print(results)
-
-
-driver.close()
-
-"""
 
 
 class Linkedin:
     def __init__(self):
         self.PATH = "./chromedriver"
+        self.FILE = config("LINKEDIN_CSV_FILE_OUTPUT")
+        self.csv_sep = "$"
         self.driver = webdriver.Chrome(self.PATH)
-        self.results = {
-            "company": None,
-            "list": []
-        }
+        self.results = []
+        self.pagination_session = 0
+        self.column_names = ["Name", 'Company',
+                             "Description", "Location", "Link"]
         self.username = config('LINKEDIN_EMAIL')
         self.password = config('LINKEDIN_PASSWORD')
 
+        self._create_csv()
         self.driver.get("https://www.linkedin.com/login")
         self._login()
 
     def _login(self):
+        """Logins to Linkedin
+        """
         username_field = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "username")))
         password_field = WebDriverWait(self.driver, 10).until(
@@ -100,84 +41,179 @@ class Linkedin:
         time.sleep(5)
 
     def search(self, search_company):
-        self.results["company"] = search_company
+        """Searches the company in Linkedin search.
+
+        Args:
+            search_company (string): Name of the company to search.
+        """
+        self.results.append({
+            "company": search_company,
+            "list": []
+        })
         search_field = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
             (By.XPATH, "//div[@id='global-nav-search']//input[@aria-label='Search']")))
+        search_field.clear()
         search_field.send_keys(search_company)
         search_field.send_keys(Keys.ENTER)
 
-    def scrap(self):
-        """Script for scrapping through the results
+    def _scroll_to_bottom(self):
+        """Scrolls the page to the bottom by pressing down key.
+
+        Returns:
+            [type]: [description]
         """
-        time.sleep(5)
-        while True:
-            body = self.driver.find_element_by_css_selector('body')
+        body = self.driver.find_element_by_css_selector('body')
+        while self.driver.execute_script('return ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2);') == False:
+            body.send_keys(Keys.PAGE_DOWN)
 
-            # Scrolls to the bottom of the page
-            while self.driver.execute_script('return ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2);') == False:
-                body.send_keys(Keys.PAGE_DOWN)
+    def _increment_pagination(self):
+        """Adds one page.
+        """
+        self.pagination_session += 1
 
-            # Scraps profiles
-            try:
+    def _reset_pagination_session(self):
+        """Resets pagination counting to 0.
+        """
+        self.pagination_session = 0
+
+    def _continue_paginating(self):
+        """Outputs whether to continue the pagination
+
+        Returns:
+            boolean: True continue, false discontinue
+        """
+        if self.pagination_session < 5:
+            return True
+        else:
+            return False
+
+    def _paginate(self):
+        """Serves the pagination of search result page. Goes to the next page unless there is no pagination or it is the last page.
+
+        Returns:
+            boolean: True if clicks next page, False if no pagination or reached the last page.
+        """
+        body = self.driver.find_element_by_css_selector('body')
+        pagination_present = len(self.driver.find_elements_by_class_name(
+            "artdeco-pagination__indicator")) > 0
+        if pagination_present:
+            next_button = self.driver.find_element_by_xpath(
+                "//button[@aria-label='Next']")
+
+            if next_button.is_enabled() and self._continue_paginating():
+                self._increment_pagination()
                 time.sleep(2)
-                result_page_items = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(
-                    (By.XPATH, "//*[@data-test-search-result='PROFILE']")))
-                for item in result_page_items:
-                    profile_object = {
-                        "name": None,
-                        "link": None,
-                        "description": None,
-                        "location": None,
-                        "full_text": item.text
-                    }
+                next_button.click()
+                time.sleep(2)
+                return True
+            else:
+                self._reset_pagination_session()
+                time.sleep(2)
+                body.send_keys(Keys.CONTROL + Keys.HOME)
+                time.sleep(2)
+                return False
+        else:
+            time.sleep(2)
+            return False
 
-                    try:
-                        wrapper = item.find_element_by_class_name(
-                            "search-result__info")
-                        name_line = wrapper.find_element_by_tag_name("a")
-                        profile_object["name"] = name_line.text.split("\n")[0]
-                        profile_object["link"] = name_line.get_attribute(
-                            "href")
-                        profile_object["description"] = wrapper.find_element_by_css_selector(
-                            "p.subline-level-1").text
-                        profile_object["location"] = wrapper.find_element_by_css_selector(
-                            "p.subline-level-2").text
-                    except:
-                        pass
+    def _get_page_data(self):
+        """Gets the results data.
 
-                    self.results["list"].append(profile_object)
+        Returns:
+            list: List of selenium objects containing results of the search.
+        """
+        time.sleep(2)
+        result_page_items = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(
+            (By.XPATH, "//*[@data-test-search-result='PROFILE']")))
+        return result_page_items
 
-                # Goes to the next page unless there is no pagination or it is the last page
-                pagination_present = len(self.driver.find_elements_by_class_name(
-                    "artdeco-pagination__indicator")) > 0
-                if pagination_present:
-                    next_button = self.driver.find_element_by_xpath(
-                        "//button[@aria-label='Next']")
+    def _unpack_page_data(self, result_page_items):
+        """Unpacks the list of selenium objects into an objects of strings. First use _get_page_data.
 
-                    if next_button.is_enabled():
-                        time.sleep(2)
-                        next_button.click()
-                        time.sleep(2)
-                    else:
-                        time.sleep(2)
-                        body.send_keys(Keys.CONTROL + Keys.HOME)
-                        time.sleep(2)
-                        break
-                else:
-                    time.sleep(2)
-                    break
+        Args:
+            result_page_items (list): List of selenium objects. Use _get_page_data method to obtain it.
+        """
+        for item in result_page_items:
+            profile_object = {
+                "name": None,
+                "link": None,
+                "description": None,
+                "location": None,
+                "full_text": item.text
+            }
+
+            try:
+                wrapper = item.find_element_by_class_name(
+                    "search-result__info")
+                name_line = wrapper.find_element_by_tag_name("a")
+                profile_object["name"] = name_line.text.split("\n")[0]
+                profile_object["link"] = name_line.get_attribute(
+                    "href")
+                profile_object["description"] = wrapper.find_element_by_css_selector(
+                    "p.subline-level-1").text
+                profile_object["location"] = wrapper.find_element_by_css_selector(
+                    "p.subline-level-2").text
             except:
                 pass
 
+            self.results[-1]["list"].append(profile_object)
+
+    def scrap(self):
+        """Scraps the data paginating in the meantime and saves it.
+        """
+        time.sleep(5)
+        while True:
+            try:
+                self._scroll_to_bottom()
+                result_page_items = self._get_page_data()
+                self._unpack_page_data(result_page_items)
+                click_next_page = self._paginate()
+                if click_next_page == False:
+                    break
+            except:
+                break
+        self.update_csv()
+
     def get_results(self):
+        """Get results.
+
+        Returns:
+            List: List of dictionaries.
+        """
         return self.results
 
-    def close_linkedin(self):
+    def get_last_to_df(self):
+        list_dict_to_df = []
+        for item in self.results[-1]["list"]:
+            dict_to_df = {}
+            dict_to_df[self.column_names[0]] = item["name"]
+            dict_to_df[self.column_names[1]] = self.results[-1]["company"]
+            dict_to_df[self.column_names[2]] = item["description"]
+            dict_to_df[self.column_names[3]] = item["location"]
+            dict_to_df[self.column_names[4]] = item["link"]
+            list_dict_to_df.append(dict_to_df)
+        df = pd.DataFrame(list_dict_to_df)
+        return df
+
+    def _create_csv(self):
+        """Create new csv file with columns.
+        """
+        df = pd.DataFrame(columns=self.column_names)
+        df.to_csv(self.FILE, sep=self.csv_sep)
+
+    def _save_to_csv(self, df):
+        """Append a DataFrame to the file.
+        """
+        df.to_csv(self.FILE, sep=self.csv_sep,
+                  mode='a', index=False, header=False)
+
+    def update_csv(self):
+        """Update csv file with the last company searched
+        """
+        df = self.get_last_to_df()
+        self._save_to_csv(df)
+
+    def close(self):
+        """Closes the selenium session.
+        """
         self.driver.close()
-
-
-x = Linkedin()
-x.search("J.P. Morgan")
-x.scrap()
-print(x.get_results())
-x.close_linkedin()
--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import pandas as pd
 from rocketreach import Rocketreach
 from hunter import Hunter
+from linkedin import Linkedin
 from decouple import config
 
 
-def Email_rocketreach(name):
+def email_rocketreach(name):
     search = Rocketreach(name)
     return search.get_results()
 
 
-def Email_hunter(domain_name):
+def email_hunter(domain_name):
     search = Hunter(domain_name)
     results = search.get_results()
     return [results["email_pattern"], results["email_list"]]
 
 
-def Populate_hunter(df):
+def populate_hunter(df):
     df[["Email Pattern", "Emails"]] = df.apply(
-        lambda x: Email_hunter(x["Domain"]), axis=1, result_type="expand")
+        lambda x: email_hunter(x["Domain"]), axis=1, result_type="expand")
     return df
 
 
-df = pd.read_excel(config('XLSX_FILE_INPUT'), index_col=0)
-df = Populate_hunter(df)
-print(df)
-df.to_excel(config('XLSX_FILE_OUTPUT'))
+def hunter_main():
+    """Main function to search for emails.
+    """
+    df = pd.read_excel(config('HUNTER_XLSX_FILE_INPUT'), index_col=0)
+    df = populate_hunter(df)
+    print(df)
+    df.to_excel(config('HUNTER_XLSX_FILE_OUTPUT'))
+
+
+def people_linkedin(linkedin_session_object, company_name):
+    linkedin_session_object.search(company_name)
+    linkedin_session_object.scrap()
+
+
+def linkedin_main():
+    linkedin_session = Linkedin()
+    df = pd.read_excel(config('LINKEDIN_XLSX_FILE_INPUT'), index_col=0)
+    df["Name"].apply(lambda name: people_linkedin(linkedin_session, name))
+    linkedin_session.close()
