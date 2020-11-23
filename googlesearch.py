f1c1b7a934c1b42dbb14a7edc71a089431a020ab
jakub-siwiec <jakub.siwiec@svcais.com>
2020-11-23 23:51:47 +0000

    Search results added

--- googlesearch.py
+++ googlesearch.py
@@ -17,7 +17,12 @@ class Domain:
         # time.sleep(5)
         self._close_google_privacy_modal()
         self.search("Goldman Sachs")
+        # self.search("ufrh3u834832ruy3yy23h")
         self.get_website_link()
+        b = self._get_search_results()
+        c = self._convert_search_results_to_dict(b)
+        print(c)
+        a = input("hehehe")
 
     def _close_google_privacy_modal(self):
         """Close initial Google privacy modal window
@@ -44,6 +49,15 @@ class Domain:
         assert "No results found." not in self.driver.page_source
         self.driver.implicitly_wait(10)
 
+    def _scroll_to_bottom(self):
+        """Scrolls the page to the bottom by pressing down key.
+        """
+        time.sleep(2)
+        body = self.driver.find_element_by_css_selector('body')
+        while self.driver.execute_script('return ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2);') == False:
+            body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+
     def _button_link(self):
         """Get the link to the button of the website (my chromedriver browser is in Polish, change "Strona" for your language)
 
@@ -61,6 +75,43 @@ class Domain:
             return None
 
         return None
+
+    def _get_search_results(self):
+        """Get all the results in the page for particular search
+
+        Returns:
+            list: list of selenium objects. None if no results
+        """
+        try:
+            self._scroll_to_bottom()
+            full_results_list = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "rc")))
+            return full_results_list
+        except:
+            return None
+
+    def _convert_search_results_to_dict(self, full_results_list):
+        """Converts results to a list of dictionaries
+
+        Args:
+            full_results_list (list): List of selenium objects
+
+        Returns:
+            list: List of dictionaries with links and titles of the search results
+        """
+        if full_results_list != None:
+            list_link_title = []
+            for result in full_results_list:
+                link = result.find_element_by_tag_name("a")
+                title = result.find_element_by_tag_name("h3")
+                result_output = {
+                    "link": link.get_attribute("href"),
+                    "title": title.text
+                }
+                list_link_title.append(result_output)
+            return list_link_title
+        else:
+            return None
 
     def get_website_link(self):
         button_link = self._button_link()
